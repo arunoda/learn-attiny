4f54d5ded1aca26726238ebc1872cd7ad9379683
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2024-11-29 10:35:36 +0530

    Add basic counter setup

--- 03-tones/src/main.cpp
+++ 03-tones/src/main.cpp
@@ -16,26 +16,52 @@ const int Note_A  = 142;
 const int Note_AS = 134;
 const int Note_B  = 127;
 
-int Speaker = PIN_PB1;
+int Speaker1 = PIN_PB1;  // Timer1 output pin
+int Speaker2 = PIN_PB0;  // Manual output pin for Timer0
 
-void TinyTone(unsigned char divisor, unsigned char octave, unsigned long duration)
-{
-  // TCCR1 = 0x90 | (8-octave); // for 1MHz clock
-  TCCR1 = 0x90 | (11-octave); // for 8MHz clock
-  OCR1C = divisor-1;         // set the OCR
-  delay(duration);
-  TCCR1 = 0x90;              // stop the counter
+volatile bool toneActive = false;
+
+void TinyTone(unsigned char divisor, unsigned char octave, unsigned long duration) {
+  TCCR1 = 0x90 | (11 - octave); // Configure Timer1 for 8 MHz clock
+  OCR1C = divisor - 1;          // Set Timer1 compare value
+  delay(duration);              // Generate the tone for the specified duration
+  TCCR1 = 0x90;                 // Stop Timer1
 }
 
-void setup()
-{
-  pinMode(Speaker, OUTPUT);
+void TinyTone2(unsigned char divisor, unsigned char octave, unsigned long duration) {
+  // Configure Timer0 in CTC mode
+  TCCR0A = (1 << WGM01);        // CTC mode
+  TCCR0B = (1 << CS00) | (octave - 1); // Set prescaler based on octave
+  OCR0A = divisor - 1;          // Set Timer0 compare value
+
+  toneActive = true;            // Activate tone output on PB0
+  delay(duration);              // Hold the tone for the duration
+  toneActive = false;           // Deactivate tone
+
+  TCCR0A = 0x00;                // Stop Timer0
+  TCCR0B = 0x00;                // Disable Timer0 clock
 }
 
-void loop()
-{ 
-  // We can also use Arduino tone() library
+ISR(TIMER0_COMPA_vect) {
+  if (toneActive) {
+    PORTB ^= (1 << Speaker2);   // Toggle PB0
+  }
+}
+
+void setup() {
+  pinMode(Speaker1, OUTPUT);
+  pinMode(Speaker2, OUTPUT);
+
+  // Enable Timer0 Compare Match Interrupt
+  TIMSK |= (1 << OCIE0A);
+}
+
+void loop() {
+  // Play tone on Speaker1 (PB1) using Timer1
   TinyTone(Note_A, 1, 500);
+
+  // // Play tone on Speaker2 (PB0) using Timer0
+  TinyTone2(Note_E, 2, 500);
+
+  // delay(500); // Pause between tones
 }
-
-
---
+++ 05-timers/src/main.cpp
@@ -0,0 +1,72 @@
+#include <avr/io.h>
+
+void setupClock() {
+  // Following two registries make the counter the normal mode.
+  // Which it counts from 0 to 255
+  TCCR0A=0x00;
+  TCCR0B=0x00;
+
+  // Ask to divide the clock by 1024
+  // To make it slower
+  // So, counter runs at 1Mhz/1024
+  // So, one tick is 1024us
+  TCCR0B |= (1<<CS00)|(1<<CS02); 
+
+  // This stores the counter itself
+  TCNT0=0;
+}
+
+// Here we wait until the counter reaches 255 
+// Since our tick is 1024us, so about 261ms
+void veryBadDelay() {
+  while (TCNT0 != 255)
+  {
+    
+  }
+
+  while (TCNT0 == 255)
+  {
+    
+  }
+}
+
+// Our counter sets an overflow bit when it reaches 255.
+// So, we can wait for it.
+void aBitBadDelay(int waitSec) {
+  long cycleTimeMs = 1024L * 255;
+  long waitCycles = (waitSec * 1000000) / cycleTimeMs;
+
+  while (waitCycles > 0)
+  {
+    // Wait until the TOV0 bit is 0
+    // This is set by the counter when it reaches to the max
+    while((TIFR & (1 << TOV0)) == 0) {
+
+    }
+    // Set the bit to 1
+    TIFR |= (1 << TOV0);
+
+    waitCycles --;
+  }
+  
+}
+
+int main() {
+  // Set the PB1 as output
+  DDRB |= (1 << PB1);
+
+  setupClock();
+
+  while(1) {
+    // make the light high.
+    PORTB |= (1 << PB1);
+    
+    aBitBadDelay(1);
+
+    // make the light low.
+    PORTB &= ~(1 << PB1);
+
+    aBitBadDelay(1);
+  }
+  return -1;
+}
