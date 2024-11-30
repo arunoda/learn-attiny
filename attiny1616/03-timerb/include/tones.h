1ee774773d631324074808f33bb00f660d138c4f
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2024-11-30 19:29:27 +0530

    Make two tones

---
+++ attiny1616/03-timerb/include/tones.h
@@ -0,0 +1,7 @@
+// Setup the tone1 which will be sent to the PA1
+void setupTone1();
+void setTone1Frequency(int freq);
+
+// Setup the tone1 which will be sent to the PA2
+void setupTone2();
+void setTone2Frequency(int freq);
--- attiny1616/03-timerb/src/main.cpp
+++ attiny1616/03-timerb/src/main.cpp
@@ -1,74 +1,31 @@
 #include <Arduino.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
-void setupTCBInterrupt() {
-    TCB0.CCMP = 65535; // Set the compare value
-    TCB0.CTRLA |= TCB_ENABLE_bm; // counting value
-
-    // Uses 20Mhz clock & divide it by for here
-    TCB0.CTRLA |= TCB_CLKSEL_CLKDIV2_gc; // clock div by 2
-  
-    // Enabling inturrupts
-    TCB0.CTRLB |= TCB_CNTMODE_INT_gc; // Timer interrupt mode (periodic interrupts)
-    TCB0.INTCTRL = TCB_CAPT_bm; // Enable Capture interrupt
-    sei();
-}
-
-// TCB0 Interrupt Service Routine
-int tcb0SubScalerCnt = 0;
-ISR(TCB0_INT_vect) {
-  // Here with 20Mhz clock speed & 2div clockdiv 
-  // we cannot acheive 20Hz
-  // That's why we are manually subscaling here
-  // NOTE: Do not use tcb0SubScalerCnt++ == 4 here. I will affect the timer
-  if (tcb0SubScalerCnt == 4) {
-    tcb0SubScalerCnt = 0;
-    PORTA.OUTTGL = PIN1_bm;
-  }
-
-  tcb0SubScalerCnt++;
-
-  // Clear the interrupt flag
-  TCB0.INTFLAGS = TCB_CAPT_bm;
-}
-
-const long CLOCK_TIME = 20000000;
-const int TIMER_DIV = 2;
-const int MANUAL_DIV = 4;
-float TICK_TIME_SEC = (1.0/(CLOCK_TIME / TIMER_DIV / MANUAL_DIV ));
-
-void setFrequency(int freq) {
-  float period = 1.0 / freq ;
-  float halfPeriod = period / 2;
-  long periodTicks = halfPeriod / TICK_TIME_SEC;
-
-  TCB0.CCMP = periodTicks;
-  // Serial.printf("Set Freq: periodUs: %ld\n", periodTicks);
-}
+#include "tones.h"
 
 void setup() {
-    // Set PA1 as output (for toggling an LED)
-    PORTA.DIRSET = PIN1_bm;
-
     // Set PA2 as input
-    PORTA.DIRCLR = PIN2_bm;
-    PORTA.PIN2CTRL = PORT_PULLUPEN_bm;
+    PORTA.DIRCLR = PIN7_bm;
+    PORTA.PIN7CTRL = PORT_PULLUPEN_bm;
 
-    // Configure TCB0 with interrupts
-    setupTCBInterrupt();
+    // Setup tones
+    setupTone1();
+    setupTone2();
 
     // Setup Serial Comm
     Serial0.begin(31250);
     Serial0.println("Sound Box Started!");
+
+    setTone1Frequency(110);
+    setTone2Frequency(120);
 }
 
 void loop() {
-    if(PORTA.IN & PIN2_bm) {
-      // When high
-      setFrequency(220);
-    } else {
-      // When low
-      setFrequency(110);
-    }
+    // if(PORTA.IN & PIN7_bm) {
+    //   // When high
+    //   setTone2Frequency(220);
+    // } else {
+    //   // When low
+    //   setTone2Frequency(110);
+    // }
 }
